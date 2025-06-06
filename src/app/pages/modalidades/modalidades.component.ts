47591a65757aab265c9a40cbdaafe4a76677ea79
Santiago Serpa <santiagoserpa902@gmail.com>
2025-06-06 10:46:07 -0500

    feature - se añadio pagina de modalidades - se disminuyo el tamaño del formulario

---
+++ src/app/pages/modalidades/modalidades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modalidades',
+  templateUrl: './modalidades.component.html',
+  styleUrl: './modalidades.component.scss'
+})
+export class ModalidadesComponent {
+
+}
