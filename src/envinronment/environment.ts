a562db542ef8cc9f97ed33b4c8d3557cc635eb91
Santiago Serpa <santiagoserpa902@gmail.com>
2025-07-03 15:18:09 -0500

    cambiamos url definitiva

--- src/envinronment/environment.ts
+++ src/envinronment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   api: {
     baseURL: 'https://familibrobackend.onrender.com',
-    contratos: 'contratos',
+    contratos: 'tecnologias',
     medicamentos: 'medicamentos',
     razonesUnicas: 'getRazonUnica'
     //authURL: 'api/v1/auth/signin',
