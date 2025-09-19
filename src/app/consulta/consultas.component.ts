7b1fe5728d4f72c1d80b9964ffc3a8c8efd15c66
santiago serpa <santiagoserpa902@gmail.com>
2025-09-19 08:58:53 -0500

    filtro desde el backend, completo con busquedas de todos los criterios

--- src/app/consulta/consultas.component.ts
+++ src/app/consulta/consultas.component.ts
@@ -5,7 +5,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { map, Observable, startWith } from 'rxjs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
@@ -14,45 +14,31 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { JsonPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
- import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
   selector: 'app-contacts',
   templateUrl: './consultas.component.html',
   styleUrls: ['./consultas.component.scss'],
-  imports:[
-       MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatInputModule,
-        MatAutocompleteModule,
-        MatFormFieldModule,
-        MatOptionModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule,
-        MatSlideToggleModule,
-        MatPaginatorModule,
-        JsonPipe,
-        MatAutocompleteModule,
-        ReactiveFormsModule,
-        MatPaginatorModule,
-        CommonModule,
-        MatProgressSpinnerModule
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    FormsModule,
+    MatSlideToggleModule,
+    JsonPipe,
+    ReactiveFormsModule,
+    CommonModule,
+    MatProgressSpinnerModule
   ]
 })
+
 export class ConsultasComponent implements OnInit {
-  razoness!: string[]; // Arreglo para almacenar las razones sociales obtenidas
-  razonCtrl = new FormControl(); // Control del formulario para el filtro
-  filteredRazones!: Observable<string[]>; // Observable para razones filtradas
-  contactsDataArray: any = [];
-  formulario!: FormGroup;
-  estados: string[] = ['ACTIVO', 'SUSPENDIDO', 'TERMINADO'];
-  contratos: string[] = ['Evento-PBS', 'Evento-NoPBS'];
-  departamentos!: string[];
-  razones!: string[];
-  dataSource = new MatTableDataSource<Contrato>();
 
   columnsToDisplay = [
     'nit',
@@ -67,20 +53,49 @@ export class ConsultasComponent implements OnInit {
     'valor',
   ];
 
+  total = 0;
+  page = 0;
+  size = 10;
+  dataSource = new MatTableDataSource<Contrato>();
+
+  /*paginator*/
+  showFirstLastButtons = true;
+  
+  //filtros iniciales
+  filtros = {
+    razon_social: '',
+    estado: '',
+    tipo: '',
+    departamento: '',
+    busquedaGeneral: ''
+  };
+
+  razoness!: string[]; // Arreglo para almacenar las razones sociales obtenidas
+  razonCtrl = new FormControl(); // Control del formulario para el filtro
+  filteredRazones!: Observable<string[]>; // Observable para razones filtradas
+  
+  //contactsDataArray: any = []; // eliminar hasta el momento
+  formulario!: FormGroup;
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-    // Obtén la referencia al elemento
+  // Obtén la referencia al elemento
   @ViewChild('inputSearch') inputSearch!: ElementRef;
+
+  estados: string[] = ['ACTIVO', 'SUSPENDIDO', 'TERMINADO'];
+  contratos: string[] = ['Evento-PBS', 'Evento-NoPBS'];
+  
+  departamentos!: string[];
+  filteredDeps!: Observable<String[]>;//deps filter
+  razones!: string[];
+  totalItems: any;
 
   constructor(
     private ContratosService: ContratosService,
     private fb: FormBuilder
-  ) {}
+  ) { }
 
-  ngOnInit() {
-    this.filtroRazones();
-    this.updateDataSource();
-
+  ngOnInit(): void {
     this.formulario = this.fb.group({
       busqueda: [''],
       estado: [''],
@@ -88,37 +103,55 @@ export class ConsultasComponent implements OnInit {
       contrato: [''],
       razon: [''],
     });
+    this.filtroRazones();
+    //this.updateDataSource();
+    this.filterDeps();
+    this.buscarContratos(); // carga inicial
   }
-
+  /****/
   ngAfterViewInit(): void {
     // Activa el foco al cargar el componente
     this.inputSearch.nativeElement.focus();
+    // Configura paginator y sort una sola vez
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
-  updateDataSource() {
-    this.ContratosService.getContratos().subscribe({
-      next: (data: Contrato[]) => {
-        // Obtener departamentos únicos
-        this.departamentos = Array.from(
-          new Set(data.map((contact) => contact.departamento))
-        );
-        //console.log(this.departamentos);
-        // Filtrar los departamentos únicos seleccionados
-        this.contactsDataArray = data.filter((contact) =>
-          this.departamentos.includes(contact.departamento)
-        );
-        // Crear el MatTableDataSource con los datos filtrados
-        this.dataSource = new MatTableDataSource<Contrato>(
-          this.contactsDataArray
-        );
+
+  loadContratos(page: number = 0, size: number = 10): void {
+    const filtros = {
+      razonSocial: (this.razonCtrl.value || '').toString(),
+      estado: (this.formulario.get('estado')?.value || '').toString(),
+      tipo: (this.formulario.get('contrato')?.value || '').toString(),
+      departamento: (this.formulario.get('departamento')?.value || '').toString(),
+      busquedaGeneral: (this.formulario.get('busqueda')?.value || '').toString()
+    };
+
+    //console.log('Enviando filtros ->', filtros, ' page:', page, ' size:', size);
+
+    this.ContratosService.getContratos(filtros, page, size).subscribe({
+      next: (response: any) => {
+        //console.log('Respuesta backend ->', response);
+        this.dataSource.data = response.data;
+        // Actualiza el total de elementos para el paginador
+        this.totalItems = response.totalItems;
+        this.page = response.page;
+        this.size = response.size;
       },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      },
+      error: err => console.error('Error al cargar contratos:', err)
     });
+  }
+
+  // buscador desde botón o submit -> reinicia la página a 0
+  buscarContratos(): void {
+    this.page = 0;
+    this.loadContratos(this.page, this.size);
+  }
+
+  onPageChange(event: PageEvent) {
+    //this.updateDataSource(event.pageIndex, event.pageSize);
+    this.page = event.pageIndex;
+    this.size = event.pageSize;
+    this.loadContratos(this.page, this.size);
   }
 
   filtroRazones() {
@@ -136,6 +169,7 @@ export class ConsultasComponent implements OnInit {
       },
     });
   }
+
   private _filterRazones(value: string): string[] {
     const filterValue = value.toLowerCase();
     return this.razoness.filter((razon) =>
@@ -143,71 +177,82 @@ export class ConsultasComponent implements OnInit {
     );
   }
 
-  applyFilter() {
-    const busqueda = this.formulario.get('busqueda')?.value;
-    const estado = this.formulario.get('estado')?.value;
-    const contrato = this.formulario.get('contrato')?.value;
-    const razon = this.razonCtrl.value; // Obtener valor del FormControl independiente
-    const departamento = this.formulario.get('departamento')?.value;
-
-    // Crear una función personalizada para el filtro
-    this.dataSource.filterPredicate = (data: Contrato, filter: string) => {
-      const filters = JSON.parse(filter);
-
-      const matchesDepartamento = filters.departamento
-        ? data.departamento
-            .toLowerCase()
-            .includes(filters.departamento.toLowerCase())
-        : true;
-
-      const matchesRazones = filters.razon
-        ? data.razonSocial.toLowerCase().includes(filters.razon.toLowerCase())
-        : true;
-
-      const matchesEstado = filters.estado
-        ? data.estado.toLowerCase().includes(filters.estado.toLowerCase())
-        : true;
-
-      const matchesBusqueda = filters.busqueda
-        ? JSON.stringify(data)
-            .toLowerCase()
-            .includes(filters.busqueda.toLowerCase())
-        : true;
-
-      const matchesContrato = filters.contrato
-        ? JSON.stringify(data)
-            .toLowerCase()
-            .includes(filters.contrato.toLowerCase())
-        : true;
-
-      // Evaluar si los datos cumplen todos los filtros aplicados
-      return (
-        matchesDepartamento &&
-        matchesEstado &&
-        matchesBusqueda &&
-        matchesContrato &&
-        matchesRazones
-      );
-    };
-
-    // Crear el objeto de filtro combinado
-    const combinedFilter = {
-      busqueda: busqueda || '',
-      estado: estado || '',
-      contrato: contrato || '',
-      departamento: departamento || '',
-      razon: razon,
-    };
-
-    // Aplicar el filtro
-    this.dataSource.filter = JSON.stringify(combinedFilter);
-
-    // filtro automatico
+  filterDeps() {
+    this.ContratosService.getDeps().subscribe({
+      next: (data: string[]) => {
+        this.departamentos = data;
+      },
+      error: (err) => console.error(err),
+    });
   }
+
+  //comentamos este filtro en el siguiente commit, este permitia filtrar pero desde el datasource(cuando cargaba toda la base de datos en la tabla)
+  // applyFilter() {
+  //   const busqueda = this.formulario.get('busqueda')?.value;
+  //   const estado = this.formulario.get('estado')?.value;
+  //   const contrato = this.formulario.get('contrato')?.value;
+  //   const razon = this.razonCtrl.value; // Obtener valor del FormControl independiente
+  //   const departamento = this.formulario.get('departamento')?.value;
+
+  //   // Crear una función personalizada para el filtro
+  //   this.dataSource.filterPredicate = (data: Contrato, filter: string) => {
+  //     const filters = JSON.parse(filter);
+
+  //     const matchesDepartamento = filters.departamento
+  //       ? data.departamento
+  //         .toLowerCase()
+  //         .includes(filters.departamento.toLowerCase())
+  //       : true;
+
+  //     const matchesRazones = filters.razon
+  //       ? data.razonSocial.toLowerCase().includes(filters.razon.toLowerCase())
+  //       : true;
+
+  //     const matchesEstado = filters.estado
+  //       ? data.estado.toLowerCase().includes(filters.estado.toLowerCase())
+  //       : true;
+
+  //     const matchesBusqueda = filters.busqueda
+  //       ? JSON.stringify(data)
+  //         .toLowerCase()
+  //         .includes(filters.busqueda.toLowerCase())
+  //       : true;
+
+  //     const matchesContrato = filters.contrato
+  //       ? JSON.stringify(data)
+  //         .toLowerCase()
+  //         .includes(filters.contrato.toLowerCase())
+  //       : true;
+
+  //     // Evaluar si los datos cumplen todos los filtros aplicados
+  //     return (
+  //       matchesDepartamento &&
+  //       matchesEstado &&
+  //       matchesBusqueda &&
+  //       matchesContrato &&
+  //       matchesRazones
+  //     );
+  //   };
+
+  //   // Crear el objeto de filtro combinado
+  //   const combinedFilter = {
+  //     busqueda: busqueda || '',
+  //     estado: estado || '',
+  //     contrato: contrato || '',
+  //     departamento: departamento || '',
+  //     razon: razon,
+  //   };
+
+  //   // Aplicar el filtro
+  //   this.dataSource.filter = JSON.stringify(combinedFilter);
+
+  //   // filtro automatico
+  // }
 
   clearFilters() {
     this.formulario.reset();
     this.dataSource.filter = '';
     this.razonCtrl.reset();
+    this.buscarContratos(); // carga inicial
   }
 }
--- src/app/services/contrato.service.ts
+++ src/app/services/contrato.service.ts
@@ -12,14 +12,34 @@ export class ContratosService {
 
   constructor(private http: HttpClient) { }
 
-  getContratos() {
-    return this.http.get<Contrato[]>(`${this.contratosUrl}/${environment.api.contratos}`);
+  // //consumir todos los contratos sin filtro
+  // getContratos(page: number, size: number) {
+  //   return this.http.get<Contrato[]>(`${this.contratosUrl}/${environment.api.contratos}?page=${page}&size=${size}`);
+  // }
+
+  //filtra la razon social con valor unico
+  getRazonesUnicas(): Observable<string[]> {
+    return this.http.get<Contrato[]>(`${environment.api.baseURL}/${environment.api.razonesUnicas}`).pipe(
+      map((contratos: Contrato[]) => contratos.map((c) => c.razonSocial))
+    );
   }
-//filtra la razon social con valor unico
-getRazonesUnicas(): Observable<string[]> {
-  return this.http.get<Contrato[]>(`${environment.api.baseURL}/${environment.api.razonesUnicas}`).pipe(
-    map((contratos: Contrato[]) => contratos.map((c) => c.razonSocial))
+  //de esta manera obtenia los deps igual que las razones
+  // getDeps(): Observable<String[]> {
+  //   return this.http.get<Contrato[]>(`${environment.api.baseURL}/${environment.api.deps}`).pipe(
+  //     map((contratos: Contrato[]) => contratos.map((c) => c.departamento))
+  //   )
+  // }
+  getDeps(): Observable<string[]> {
+    return this.http.get<string[]>(`${environment.api.baseURL}/${environment.api.deps}`);
+  }
+
+
+  getContratos(filtros: any, page: number, size: number) { 
+  return this.http.post<any>(
+    `${this.contratosUrl}/tecnologias/search?page=${page}&size=${size}`,
+    filtros
   );
 }
+
 
 }
